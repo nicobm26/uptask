13f6f24e23814c688308b0ae7e8ba643b3b4b044
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-16 21:00:27 -0500

    estructura

---
+++ includes/app.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Conectarnos a la base de datos
+use Model\ActiveRecord;
+
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+require 'funciones.php';
+require 'database.php';
+
+ActiveRecord::setDB($db);
