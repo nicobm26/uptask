1787701721a60465393b712770dce389306b1623
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-18 13:37:12 -0500

    agregando el render a login y a crear

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,27 +1,34 @@
 <?php
 
 namespace Controllers;
+use MVC\Router;
 
 class LoginController {
 
-    public static function login() {
-        echo "desde login";
-
+    public static function login(Router $router) {
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         }
+
+        //Render a la vista
+        $router->render('auth/login',[
+
+        ]);
     }
 
     public static function logout() {
         echo "desde logout";    
     }
 
-    public static function crear() {
-        echo "desde crear";
+    public static function crear(Router $router) {
 
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         }
+
+        $router->render('auth/crear',[
+            'titulo' => 'Crea tu cuenta en UpTask'
+        ]);
     }
 
     public static function olvide() {
