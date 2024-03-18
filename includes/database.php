8bf9c7ccd3eeb95202cca2eaf1c79f341e860a19
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-18 11:30:01 -0500

    cargo las variables de entrono

--- includes/database.php
+++ includes/database.php
@@ -1,6 +1,10 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', '', '');
+$db = mysqli_connect(
+        $_ENV['DB_HOST'], 
+        $_ENV['DB_USER'], 
+        $_ENV['DB_PASS'], 
+        $_ENV['DB_NAME']);
 
 
 if (!$db) {
