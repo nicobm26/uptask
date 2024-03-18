212e384044b0debe3f4a872ce260b654b3589c3e
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-18 10:05:42 -0500

    Estructura del routing y de los metodos del LoginController

--- public/index.php
+++ public/index.php
@@ -2,10 +2,28 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 $router = new Router();
 
 
+//Login
+$router->get( '/' , [LoginController::class, 'login']);
+$router->post( '/' , [LoginController::class, 'login']);
+$router->post( '/logout' , [LoginController::class, 'logout']);
+
+//Creacion de cuentas
+$router->get( '/crear' , [LoginController::class, 'crear']);
+$router->post( '/crear' , [LoginController::class, 'crear']);
+    //formulario de olvide mi contraseña
+$router->get( '/olvide' , [LoginController::class, 'olvide']);
+$router->post( '/olvide' , [LoginController::class, 'olvide']);
+    //Colocar el nuevo password
+$router->get( '/reestablecer' , [LoginController::class, 'reestablecer']);
+$router->post( '/reestablecer' , [LoginController::class, 'reestablecer']);
+    //confirmacion cuenta
+$router->get( '/mensaje' , [LoginController::class, 'mensaje']);
+$router->get( '/confirmar' , [LoginController::class, 'confirmar']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
