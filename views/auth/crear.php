e63c47ba211011694df55f29450eca5a687d1b20
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-18 14:30:27 -0500

    vista de crear

---
+++ views/auth/crear.php
@@ -0,0 +1,59 @@
+<div class="contenedor crear">
+
+<?php
+    include_once(__DIR__ ."/../templates/nombre-sitio.php");
+?>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Crea tu Cuenta</p>
+
+        <form class="formulario" method="post">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input 
+                    type="text" 
+                    id="nombre" 
+                    name="nombre"
+                    placeholder="Tu Nombre"
+                />
+            </div>
+
+            <div class="campo">
+                <label for="email">Correo</label>
+                <input 
+                    type="email" 
+                    id="email" 
+                    name="email"
+                    placeholder="Tu Correo"
+                />
+            </div>
+
+            <div class="campo">
+                <label for="password">Contraseña</label>
+                <input 
+                    type="password" 
+                    id="password" 
+                    name="password"
+                    placeholder="Tu Contraseña"
+                />
+            </div>
+
+            <div class="campo">
+                <label for="password2">Repetir Contraseña</label>
+                <input 
+                    type="password" 
+                    id="password2" 
+                    name="password2"
+                    placeholder="Repite tu Contraseña"
+                />
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+
+            <div class="acciones">
+                <a href="/">¿Ya tienes una cuenta? Iniciar Sesión</a>
+                <a href="/olvide">¿Olvidaste tu Contraseña?</a>
+            </div>
+        </form>
+    </div> <!-- .contenedor-sm-->
+</div> <!-- .contenedor-->
