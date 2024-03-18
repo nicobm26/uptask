f07727ca9556684e8c452452058dac941428eb0d
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-18 17:53:51 -0500

    agrego el modelo usuario

---
+++ models/Usuario.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Model;
+
+class Usuario extends ActiveRecord{
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'nombre', 'email', 'password', 'token', 'confirmado'];
+
+    public $id;
+    public $nombre;
+    public $email;
+    public $password;
+    public $password2;
+    public $token;
+    public $confirmado;
+
+    public function __construct($args = []){
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? '';
+        $this->token = $args['token'] ?? '';
+        $this->confirmado = $args['confirmado'] ?? 0;
+    }
+
+
+}
