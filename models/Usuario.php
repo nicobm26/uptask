345113c9ad2fe672260d71ed160389d98658351a
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-18 18:45:17 -0500

    Agrego metodo validarCampos

--- models/Usuario.php
+++ models/Usuario.php
@@ -24,5 +24,32 @@ class Usuario extends ActiveRecord{
         $this->confirmado = $args['confirmado'] ?? 0;
     }
 
+    //Validacion para cuentas nuevas
+    public function validarCampos(){
+
+        if(!$this->nombre){
+            self::$alertas['error'][] = 'El nombre es obligatorio';
+        }else if (! preg_match("/^[a-zA-Z ]+$/", $this->nombre)){
+            self::$alertas['error'][] = 'El nombre no puede llevar numeros o caraceteres especiales';
+        }
+
+        if(empty($this->email) ){
+            self::$alertas['error'][] = 'El Correo es obligatorio';
+        }else if(!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            self::$alertas['error'][] = 'El Correo no tiene el formato correcto';
+        }
+
+        if(!$this->password || !$this->password2){
+            self::$alertas['error'][] = 'La contraseña es obligatoria';
+        }else if( !preg_match("/^(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){5,})(?=(?:.*[0-9]){1})/", $this->password) ||  !preg_match("/^(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){5,})(?=(?:.*[0-9]){1})/", $this->password2)){
+            self::$alertas['error'][] = "La contraseña no es válida. Debe contener al menos 5 letras minúsculas, un número y una letra mayúscula.";
+        }
+        
+        if($this->password2 && $this->password && $this->password != $this->password2){
+            self::$alertas["error"][] = 'Las contraseñas son diferentes';
+        }
+
+        return self::$alertas;
+    }
 
 }
