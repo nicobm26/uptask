54f3a62441fd081653c9c74fe7201dfdcf938270
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-20 14:27:31 -0500

    agregue el template de alertas y modifique algunos textos

--- views/auth/reestablecer.php
+++ views/auth/reestablecer.php
@@ -6,7 +6,13 @@
  <div class="contenedor-sm">
         <p class="descripcion-pagina">Coloca tu Nueva Contraseña</p>
 
-        <form class="formulario" method="post" action="/reestablecer">
+        <?php
+            include_once(__DIR__ ."/../templates/alertas.php");
+
+            if(!$mostrar) return;
+        ?>
+
+        <form class="formulario" method="post">
          
             <div class="campo">
                 <label for="password">Contraseña</label>
@@ -18,7 +24,7 @@
                 />
             </div>
 
-            <input type="submit" class="boton" value="Iniciar Sesión">
+            <input type="submit" class="boton" value="Guardar Nueva Contraseña">
 
             <div class="acciones">
                 <a href="/crear">¿Aún no tienes una cuenta? Crea una</a>
