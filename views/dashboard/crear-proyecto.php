a3bc5a33a3a59539ceb0dc40680b7cb4c8cae5c5
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-20 18:42:46 -0500

    creo el contenido que tendra la vista crear-proyecto

--- views/dashboard/crear-proyecto.php
+++ views/dashboard/crear-proyecto.php
@@ -3,7 +3,22 @@
 ?>
 
 
+<div class="contenedor-sm">
+    <?php
+        include_once(__DIR__ ."/../templates/alertas.php");
+    ?>
 
+    <form class="formulario" action="">
+
+        <?php
+            include_once(__DIR__ ."/formulario-proyecto.php");
+        ?>
+
+        <input type="submit" value="Crear Proyecto">
+    </form>
+
+
+</div>
 
 
 <?php
---
+++ views/dashboard/formulario-proyecto.php
@@ -0,0 +1,9 @@
+<div class="campo">
+    <label for="nombre">Nombre del Proyecto</label>
+    <input 
+        type="text"
+        id="proyecto"
+        name="proyecto"
+        placeholder="Nombre del proyecto"
+    />
+</div>
