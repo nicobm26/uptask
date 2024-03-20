fc28e6d93115c4d0c05eb2b7dc65fd0996e84215
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-20 16:53:54 -0500

    vista de sidebar

---
+++ views/templates/sidebar.php
@@ -0,0 +1,9 @@
+<aside class="sidebar">
+    <h2>UpTask</h2>
+
+    <nav class="sidebar-nav">
+        <a href="/dashboard">Proyectos</a>
+        <a href="/">Crear Proyecto</a>
+        <a href="/perfil">Perfil</a>
+    </nav>
+</aside>
