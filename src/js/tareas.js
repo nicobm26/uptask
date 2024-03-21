30737dcae549ac51c150c1f65aee22f268e1c303
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-21 17:09:54 -0500

    creando el model para añadir tareas

---
+++ src/js/tareas.js
@@ -0,0 +1,37 @@
+( function(){
+    
+    //boton para mostrar el formulario
+    let nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario(){
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+        <form class='formulario nueva-tarea'>
+            <legend>Añade una nueva tarea</legend>
+            <div class='campo'>
+                <label>Tarea</label>
+                <input
+                    type='text'
+                    name='tarea'
+                    id='tarea'
+                    placeholder='Añadir Tarea al Proyecto Actual'
+                /> 
+            </div>
+            <div class='opciones'>
+                <input 
+                    type='submit'
+                    class='submit-nueva-tarea'
+                    value='Añadir Tarea'
+                />
+                <button type='button' class='cerrar-modal'>Cancelar</button>
+            </div>
+        </form>        
+        `;
+
+        document.querySelector('body').appendChild(modal);
+    }
+})();
+
+
