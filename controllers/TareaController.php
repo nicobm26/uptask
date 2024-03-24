64ff395f8ff3aba123c9fd37fef536a9d45b7adf
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-24 13:29:10 -0500

    metodo de crear de TareaController

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -1,6 +1,9 @@
 <?php
 
-namespace Model;
+namespace Controllers;
+
+use Model\Proyecto;
+use Model\Tarea;
 
 class TareaController{
     public static function index(){
@@ -9,8 +12,33 @@ class TareaController{
     }
 
     public static function crear(){
+        session_start();
+        // debuguear($_POST);
 
+        $urlProyecto = $_POST['url'];
+        $proyecto = Proyecto::where('url', $urlProyecto);
+        // $respuesta = json_encode($proyecto);
+        if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']){
+            $respuesta = [
+                'tipo' => 'error',
+                'mensaje' => 'Hubo un error al agregar la tarea'
+            ];
+            echo json_encode($respuesta);        
+        }else{
+            $tarea = New Tarea($_POST);
+            $tarea->proyectoId = $proyecto->id;
+            $resultado = $tarea->guardar();            
+            $respuesta = [
+                'tipo' => 'exito',
+                'mensaje' => 'Tarea agregada Correctamente',
+                'id' => $resultado['id']
+            ];
+            echo json_encode($respuesta);    
+        }
+
+        //Todo bien, instanciar y crear la tarea
         
+               
     }
 
     public static function actualizar(){
