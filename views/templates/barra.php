282d4a425be255b55f8ae6018a0b6a19e633685a
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-28 13:32:44 -0500

    ocultando panales para que sea responsive

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,3 +1,10 @@
+<div class="barra-mobile">
+    <h1>UpTask</h1>
+    <div class="menu">
+        <img src="build/img/menu.svg" alt="Imagen del Menu">
+    </div>
+</div>
+
 <div class="barra">
     <p>Hola: <span><?php echo ucfirst($_SESSION['nombre']) ?></span></p>
 
