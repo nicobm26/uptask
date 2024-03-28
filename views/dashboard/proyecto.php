39e6f057a8b562d380e587874da11516fda5b06e
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-28 13:47:11 -0500

    concatenando

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -58,7 +58,7 @@
 ?>
 
 <?php
-    $script="
+    $script .="
         <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
         <script src='build/js/tareas.js'></script>
     ";
