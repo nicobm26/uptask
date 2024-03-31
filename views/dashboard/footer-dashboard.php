5db368b12f7e839e0d603380b7cd019077e4035d
nicolasb <nicolasbotellomendoza@gmail.com>
2024-03-31 18:56:09 -0500

    quitar un espacio

--- views/dashboard/footer-dashboard.php
+++ views/dashboard/footer-dashboard.php
@@ -5,7 +5,6 @@
 <?php
     $script ="
         <script src='/build/js/nav.js'> </script>
-        <script src='/build/js/app.js'> </script>
-
+        <script src='/build/js/app.js'> </script>        
     "
 ?>
