341cc83b65e44e42c34d8b60bc22cfe230839fcf
nicolasb <nicolasbotellomendoza@gmail.com>
2024-04-01 11:16:41 -0500

    agrego el input hidden para eliminar proyect

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -10,7 +10,7 @@
         <?php foreach ($proyectos as $proyecto) {?>
             <li class="proyecto">
                 <form action="/proyecto/eliminar" method="post">
-                    <input type="hidden" name="id" value="<?php echo $proyecto->id ?>">;
+                    <input type="hidden" name="id" value="<?php echo $proyecto->id ?>">
                     <input type="submit" class="btn-eliminar" value="&times;">                    
                 </form>                
                 <a href="/proyecto?id=<?php echo $proyecto->url; ?>"> <?php echo $proyecto->nombre; ?></a>
@@ -22,7 +22,7 @@
 
 <?php
     include_once(__DIR__ ."/footer-dashboard.php");
-    $script = "
+    $script .= "
     <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
     <script src='build/js/eliminarProyecto.js'> </script>"
 ?>
