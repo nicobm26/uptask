20e60b666d785afc8d5f46bb85a1792a98911b00
nicolasb <nicolasbotellomendoza@gmail.com>
2024-04-01 13:32:11 -0500

    js para el dark mode

---
+++ src/js/darkMode.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function(){  
+    darkMode();
+})
+
+function darkMode(){
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
+    console.log(prefiereDarkMode)
+
+    if(prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode')
+    }else{
+        document.body.classList.remove('dark-mode')
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode')
+        }else{
+            document.body.classList.remove('dark-mode')
+        }
+    })
+
+    const botonDarkMode = document.querySelector(".dark-mode-boton");
+
+    // if(!botonDarkMode)
+        // return;
+
+    botonDarkMode.addEventListener('click', function(){
+        document.body.classList.toggle('dark-mode')
+          //Para que el modo elegido se quede guardado en local-storage
+          if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('modo-oscuro','true');
+        } else {
+            localStorage.setItem('modo-oscuro','false');
+        }
+    })
+
+    //Obtenemos el modo del color actual
+    if (localStorage.getItem('modo-oscuro') === 'true') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
+
